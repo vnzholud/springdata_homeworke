f58bb7eb63138bd70efb166c9d5265e570e53e82
ZholudV <goldexzholud@gmail.com>
2022-09-10 03:10:21 +0500

    добавлены: контроллеры, сущности, репозитории, сервисы исправлены ошибки добавлены 20 продуктов

---
+++ src/main/java/ru/zholud/springdata_homeworke/controlers/ProductController.java
@@ -0,0 +1,46 @@
+package ru.zholud.springdata_homeworke.controlers;
+
+
+import ru.zholud.springdata_homeworke.exeptions.ResourceNotFoundException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import ru.zholud.springdata_homeworke.entities.Product;
+import ru.zholud.springdata_homeworke.services.ProductService;
+
+import java.util.List;
+
+@RestController
+public class ProductController {
+    private ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/products")
+    public List<Product> findAll() {
+        return productService.findAll();
+    }
+
+    @GetMapping("/products/{id}")
+    public Product findById(@PathVariable Long id) {
+        return productService.findById(id).orElseThrow(() -> new ResourceNotFoundException("Product not found, id: " + id));
+    }
+
+    @GetMapping("/products/delete/{id}")
+    public void deleteById(@PathVariable Long id) {
+        productService.deleteById(id);
+    }
+
+    @GetMapping("/products/change_score")
+    public void changeScore(@RequestParam Long studentId, @RequestParam Integer delta) {
+        productService.changeScore(studentId, delta);
+    }
+
+    @GetMapping("/products/score_between")
+    public List<Product> findStudentsByScoreBetween(@RequestParam(defaultValue = "0") Integer min, @RequestParam(defaultValue = "100") Integer max) {
+        return productService.findByPriceBetween(min, max);
+    }
+}
---
+++ src/main/java/ru/zholud/springdata_homeworke/entities/Product.java
@@ -0,0 +1,48 @@
+package ru.zholud.springdata_homeworke.entities;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "price")
+    private Integer price;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public Product() {
+    }
+}
