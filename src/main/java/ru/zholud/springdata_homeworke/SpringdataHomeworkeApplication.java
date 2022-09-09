e584a962881d4a4d7d8c37b320182cd16ba33d81
ZholudV <goldexzholud@gmail.com>
2022-09-09 18:54:43 +0500

    первый комит

---
+++ src/main/java/ru/zholud/springdata_homeworke/SpringdataHomeworkeApplication.java
@@ -0,0 +1,13 @@
+package ru.zholud.springdata_homeworke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdataHomeworkeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringdataHomeworkeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/zholud/springdata_homeworke/SpringdataHomeworkeApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.zholud.springdata_homeworke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdataHomeworkeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
